487e8775a5bce0ec90348b448a3c45150fcacf9f
H1pen <lemannegropi@gmail.com>
2026-04-05 23:54:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.filters import Command
 # --- НАСТРОЙКИ ---
 TOKEN = "8668356633:AAE29U5g3PcT8r8eYOM_zhmXWtRa_QVQvQo"
 ADMIN_GROUP_ID = -1003670917930 
-CHANNEL_ID = "@WeSchoolPodslyshano" 
+CHANNEL_ID = "@WeSchoolPodslyshano"
 
 class SlowModeMiddleware(BaseMiddleware):
     def __init__(self):
