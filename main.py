e76eede2d132820193c44e42766c1a06f8c0186d
H1pen <lemannegropi@gmail.com>
2026-04-07 20:24:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from aiogram.filters import Command
 
 # --- НАСТРОЙКИ ---
 TOKEN = "8668356633:AAE29U5g3PcT8r8eYOM_zhmXWtRa_QVQvQo"
-ADMIN_GROUP_ID = -1008668356633
+ADMIN_GROUP_ID = -1003670917930
 CHANNEL_ID = -1003742221408 
 
 # Middleware для ограничения отправки (раз в 60 секунд)
