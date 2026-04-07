aecc7005f96d8887f65b3a4a0a67f028d0c3fcc6
H1pen <lemannegropi@gmail.com>
2026-04-07 20:23:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from aiogram.filters import Command
 # --- НАСТРОЙКИ ---
 TOKEN = "8668356633:AAE29U5g3PcT8r8eYOM_zhmXWtRa_QVQvQo"
 ADMIN_GROUP_ID = -1008668356633
-CHANNEL_ID = "@WeSchoolPodslyshano" 
+CHANNEL_ID = -1003742221408 
 
 # Middleware для ограничения отправки (раз в 60 секунд)
 class SlowModeMiddleware(BaseMiddleware):
